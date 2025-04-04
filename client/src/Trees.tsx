c9c5961a30f5c33a8d4c8cbcaa90153298255393
Hannah Wischnia <hwischnia@seatgeek.com>
2025-04-03 23:16:14 -0400

    basic mobile optimizations

--- client/src/Trees.tsx
+++ client/src/Trees.tsx
@@ -2,11 +2,11 @@ export const Trees = ({ fullWidth = 600 }: { fullWidth?: number }) => {
   const width = fullWidth / 6;
   return (
     <div className="treeWrapper">
-      <img src="/tree-1.png" width={width} />
+      <img className="just-desktop" src="/tree-1.png" width={width} />
       <img src="/tree-2.png" width={width} />
-      <img src="/tree-3.png" width={width} />
+      <img className="just-desktop" src="/tree-3.png" width={width} />
       <img src="/tree-4.png" width={width} />
-      <img src="/tree-6.png" width={width} />
+      <img className="just-desktop" src="/tree-6.png" width={width} />
       <img src="/tree-5.png" width={width} />
     </div>
   );
