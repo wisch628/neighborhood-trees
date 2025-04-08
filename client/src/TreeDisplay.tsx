177a7a410b3ef1abc488905d50cb0d30a931b9bf
Hannah Wischnia <hwischnia@seatgeek.com>
2025-04-07 21:06:23 -0400

    remove animation

--- client/src/TreeDisplay.tsx
+++ client/src/TreeDisplay.tsx
@@ -2,7 +2,7 @@ import { Tree } from "./types";
 import { Trees } from "./Trees";
 import { useCount } from "./useCounter";
 import { SubTree } from "./SubTree";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const TreeDisplay = ({
   trees,
@@ -11,17 +11,7 @@ export const TreeDisplay = ({
   trees: Tree[];
   clearResults: () => void;
 }) => {
-  const [showTrees, setShowTrees] = useState(false);
   const [selectedTree, setSelectedTree] = useState(trees[0]);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowTrees(true); // Set state to true after 3 seconds
-    }, 3000); // 3000ms = 3 seconds
-
-    // Cleanup the timeout if the component unmounts
-    return () => clearTimeout(timer);
-  }, []); // Empty dependency array ensures this runs only once when the component mounts
 
   const [topTree, ...otherTrees] = trees;
   const count = useCount(topTree.count);
@@ -29,7 +19,7 @@ export const TreeDisplay = ({
     <div className="flex-col">
       <Trees fullWidth={300} />
       <h3>Your zip code has...</h3>
-      <h1>
+      <h1 onClick={() => setSelectedTree(topTree)}>
         <span className="tree-count">{count} </span>
         {topTree?.common}s
       </h1>
@@ -50,14 +40,10 @@ export const TreeDisplay = ({
       <figcaption>
         {selectedTree.species}, better known as {selectedTree.common}
       </figcaption>
-      {showTrees && (
-        <>
-          <h3>
-            Click on the tree name to see a photo of it generated from the
-            Wikipedia API, or <a onClick={clearResults}>try a new zip code.</a>
-          </h3>
-        </>
-      )}
+      <h3>
+        Click on the tree name to see a photo of it generated from the Wikipedia
+        API, or <a onClick={clearResults}>try a new zip code.</a>
+      </h3>
     </div>
   );
 };
